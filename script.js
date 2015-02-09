f55fbd0fc6a92234e68ef4b6a12e849f002559d1
Ian Fajardo <Ian.Fajardo@blackbaud.com>
2015-02-09 15:18:13 -0500

    Modularization and Further jQuery additoins
    Alert editing

--- script.js
+++ script.js
@@ -1,45 +1,44 @@
 (function(){
 	$("#myForm").submit(function(event){
-				event.preventDefault();
-				var form = $( this )
-				var url = form.attr('action');
+		event.preventDefault();
+		var form = $( this )
+		var url = form.attr('action');
 
-				
-				var posting = $.post( url, { 
-					date: $('#date').val(), 
-					time: $('#time').val(), 
-					minute: $('#minute option:selected').val(), 
-					hour: $('#hour option:selected').val(), 
-					period: $('#period option:selected').val(), 
-					cost: $('#cost').val(), 
-					city: $('#city').val(), 
-					state: $('#state option:selected').val(),
-					venue: $('#venue').val()
-				});
+		var valArr = { 
+			date: $('#date').val(), 
+			time: $('#time').val(), 
+			minute: $('#minute option:selected').val(), 
+			hour: $('#hour option:selected').val(), 
+			period: $('#period option:selected').val(), 
+			cost: $('#cost').val(), 
+			city: $('#city').val(), 
+			state: $('#state option:selected').val(),
+			venue: $('#venue').val()
+		};
 
-				if(
-
-					isNaN(cost)
-
-
-					){
-					var errorString = '';
-					errorString += ( isNaN(cost) ? "Error: Cost must be a Number" : '');
-					$('.error').html(errorString);
-					$('.error').show();
-					$("#cost").val("");
-					location.hash = "form-container";
-				}
-				else{
-					posting.success(function(data){
-						$('.results').html(
-							data + " <a href='#'>View Homepage</a>");
-						$('.results').show();
-						form[0].reset();
-					});
-				}
-
+		//client-side error handling
+		if( !$.isNumeric(valArr.cost)){
+			$('.results').hide();
+			var errorString = '';
+			errorString += ( isNaN(cost) ? "Error: Cost must be a Number" : '');
+			$('.error').html(errorString);
+			$('.error').show();
+			$("#cost").val("");
+			location.hash = "form-container";
+		}
+		//post to script if inputs are correct
+		else{
+			var posting = $.post( url, valArr);
+			posting.success(function(data){
+				$('.error').hide();
+				$('.results').html(
+					data + " <a href='http://lildarkmark.com'>View Homepage</a>");
+				$('.results').show();
+				form[0].reset();
 			});
+		}
+
+	});
 
 	$("#free_checkbox").change(function(){
 		var c = this.checked ? '0' : '';
@@ -53,7 +52,6 @@
 	});
 
 	$("#cost").blur(function(){
-		console.log("changed");
 		var val = $(this).val();
 		if(val != '0'){
 			$("#free_checkbox").prop("checked", false);
@@ -64,4 +62,4 @@
 		}
 	});
 
-})();
+})(jQuery);
